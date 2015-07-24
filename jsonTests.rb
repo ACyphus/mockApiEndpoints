0376b7f55163220a17f8c75cfc39039895ff0daa
acyphus <a.cyphus@gmail.com>
2015-07-24 15:36:30 +0100

    Add nested JSON endpoint
    - Use static data for a multi level nested JSON payload

--- jsonTests.rb
+++ jsonTests.rb
@@ -10,3 +10,21 @@ get '/notEmpty.json' do
   content_type :json
   { :key => "value", :key2 => 'value' }.to_json
 end
+
+get '/nested.json' do
+  content_type :json
+  {
+    :organisation => {
+      'name' => 'Big Bad Wolf',
+      'people' => {
+        0 => {
+          'name' => 'Alex Cyphus'
+        },
+        1 => {
+          'name' => 'Joe Bloggs'
+        }
+      },
+      'pets' => {}
+    }
+  }.to_json
+end
